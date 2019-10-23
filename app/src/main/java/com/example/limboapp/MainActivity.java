3a91fb1b5fa53af5f906fc0bd77c9a2ba03d702d
ducknguyen <duchnguy@ucsc.edu>
2019-10-23 13:51:48 -0700

    Updated starting activity to LogInActivity

--- app/src/main/java/com/example/limboapp/MainActivity.java
+++ app/src/main/java/com/example/limboapp/MainActivity.java
@@ -15,18 +15,5 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        setContentView(R.layout.activity_log_in);
-
-
-
-
-            // The way to move from one activity to the next is to use "intent"
-            // Only makes an intent, not executing it.
-            Intent intent = new Intent(getApplicationContext(), LogInActivity.class);
-
-            // Execute intent
-            startActivity(intent);
-
-
     }
 }
