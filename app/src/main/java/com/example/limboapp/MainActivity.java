66e0776bb18da7245686d5c22ef77d7de4968d05
Paige <spam.paige@gmail.com>
2019-10-24 16:21:51 -0700

    Added Bottom Navigation and Fragments to MainActivity

--- app/src/main/java/com/example/limboapp/MainActivity.java
+++ app/src/main/java/com/example/limboapp/MainActivity.java
@@ -1,19 +1,69 @@
 package com.example.limboapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.limboapp.dummy.DummyContent;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener,
+        HomeFragment.OnListFragmentInteractionListener,
+        RecordFragment.OnFragmentInteractionListener,
+        UserFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        Log.i("DEBUG:", "App launched into MainActivity");
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = findViewById(R.id.main_navi);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
+    }
+
+    //checks if given fragment exists, and loads it if possible
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frame_layout,fragment).commit();
+            return true;
+        }
+        return false;
+    }
+
+    //switches between fragments for Main Activity
+    //based on what the user has tapped on
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch(menuItem.getItemId()) {
+            case R.id.navigation_home:
+                fragment = HomeFragment.newInstance();
+                break;
+            case R.id.navigation_record:
+                fragment = RecordFragment.newInstance();
+                break;
+            case R.id.navigation_user:
+                fragment = UserFragment.newInstance();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
